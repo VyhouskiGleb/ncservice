41a7f556bc83e23acaf7a7cf109f3dbebdd555de
gleby44 <vyhouski.gleb@gmail.com>
2019-10-25 16:48:36 +0300

    Revert "initial ethalon commit"
    This reverts commit 84c748836fbcc3211619feaedb0b44c17accab39.

--- parser/public/indexController/indexController.js
+++
@@ -1,6 +0,0 @@
-module.exports = class authController {
-    controllerRun(app, db, rote, req, res) {
-        res.send(JSON.stringify('indexController'));
-        return true;
-    }
-}
--- startBEJAR.bat
+++
@@ -1,2 +0,0 @@
-start cmd.exe /k "java -jar ./be\target\backend-0.0.1-SNAPSHOT.jar"
-start cmd.exe /k "java -jar ./fapi\target\fapi-0.0.1-SNAPSHOT.jar"
